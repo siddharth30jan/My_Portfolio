45a4977d58460481e1f80cf315aa10720c2f370d
Siddharth Singh <siddharths51700@gmail.com>
2020-09-22 19:11:57 +0530

    Updates

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ if (theme == null) {
 
 let themeDots = document.getElementsByClassName("theme-dot");
 
-for (var i = 0; themeDots.length > i; i++) {
+for (let i = 0; themeDots.length > i; i++) {
   themeDots[i].addEventListener("click", function () {
     let mode = this.dataset.mode;
     console.log("Option clicked:", mode);
